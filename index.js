5b334b1e35ec631f9fd07af6ad12de1c82877e13
Leandro Heffes <leandroheffes@outlook.es>
2024-02-27 10:05:48 -0300

    basic structure

---
+++ index.js
@@ -0,0 +1,26 @@
+// -------------------- REQUIREMENTS --------------------
+
+const express = require("express")
+const mainRouter = require("./src/routers/mainRouter")
+
+// -------------------- APP --------------------
+
+const app = express()
+
+// -------------------- SETTINGS --------------------
+
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/src/views")
+
+// -------------------- USES --------------------
+
+app.use(express.static(__dirname + "/public"))
+
+// -------------------- ROUTERS --------------------
+
+app.use(mainRouter)
+
+// -------------------- SERVER --------------------
+
+const PORT = process.env.PORT || 3024
+app.listen(PORT, () => console.log(`running server on port ${PORT}`))
---
+++ src/routers/mainRouter.js
@@ -0,0 +1,16 @@
+// -------------------- REQUIREMENTS --------------------
+
+const {Router} = require("express")
+const mainController = require("../controllers/mainController")
+
+// -------------------- ROUTER --------------------
+
+const router = Router()
+
+// -------------------- ROUTES --------------------
+
+router.get("/", mainController.index)
+
+// -------------------- EXPORT --------------------
+
+module.exports = router
