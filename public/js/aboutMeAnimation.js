d0dfc1a555719d00e0bd98f4341f1cbaf32ea1b8
Leandro Heffes <leandroheffes@outlook.es>
2024-03-02 10:52:56 -0300

    navbarMenu corrections and about me animation added for mobile

---
+++ public/js/aboutMeAnimation.js
@@ -0,0 +1,13 @@
+const aboutMeInfoContainer = document.querySelector('.aboutMeInfoContainer');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            aboutMeInfoContainer.classList.add('aboutMeAnimation');
+        } else {
+            aboutMeInfoContainer.classList.remove('aboutMeAnimation');
+        }
+    });
+});
+
+observer.observe(aboutMeInfoContainer);
