2057f3727e3382eafd7e5b2b9a22e64fb437bc06
Leandro Heffes <leandroheffes@outlook.es>
2024-03-04 13:16:34 -0300

    mini desktop view complete

--- public/js/proyectDetail.js
+++ public/js/proyectDetail.js
@@ -2,6 +2,8 @@ const swProyectContainer = document.getElementById("swProyectContainer")
 const swProyectDetail = document.getElementById("swProyectDetail")
 const clothingEcommerceContainer = document.getElementById("clothingEcommerceContainer")
 const clothingEcommerceProyectDetail = document.getElementById("clothingEcommerceProyectDetail")
+const theWeekndPortalContainer = document.getElementById("theWeekndPortalContainer")
+const theWeekndPortalDetail = document.getElementById("theWeekndPortalDetail")
 const closeProyectDetailButton = document.querySelector(".closeProyectDetailButton")
 
 swProyectContainer.addEventListener("click", (e) => {
@@ -15,5 +17,12 @@ clothingEcommerceContainer.addEventListener("click", (e) => {
     clothingEcommerceProyectDetail.classList.toggle("display-block")
     closeProyectDetailButton.addEventListener("click", (e) => {
         clothingEcommerceProyectDetail.classList.toggle("display-block")
+    })
+})
+
+theWeekndPortalContainer.addEventListener("click", (e) => {
+    theWeekndPortalDetail.classList.toggle("display-block")
+    closeProyectDetailButton.addEventListener("click", (e) => {
+        theWeekndPortalDetail.classList.toggle("display-block")
     })
 })
