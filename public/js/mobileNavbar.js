bfc1160ce3e380de017fe81c0a3332e1423d181c
Leandro Heffes <leandroheffes@outlook.es>
2024-03-04 11:05:59 -0300

    tablet view complete

--- public/js/mobileNavbar.js
+++ public/js/mobileNavbar.js
@@ -1,10 +1,45 @@
+// const mobileNavbarButton = document.querySelector(".mobileNavbarButton")
+// const mobileNavbar = document.querySelector(".mobileNavbar")
+// const closeMobileNavbarButton = document.querySelector(".closeMobileNavbarButton")
+// const mobileNavbarLink1 = document.getElementById("mobileNavbarLink1")
+// const mobileNavbarLink2 = document.getElementById("mobileNavbarLink2")
+// const mobileNavbarLink3 = document.getElementById("mobileNavbarLink3")
+// const mobileNavbarLink4 = document.getElementById("mobileNavbarLink4")
+
+// mobileNavbarButton.addEventListener("click", (e) => {
+//     mobileNavbar.classList.toggle("display-block")
+//     mobileNavbarButton.classList.toggle("display-none")
+// })
+
+// closeMobileNavbarButton.addEventListener("click", (e) => {
+//     mobileNavbar.classList.toggle("display-block")
+//     mobileNavbarButton.classList.toggle("display-none")
+// })
+
+// mobileNavbarLink1.addEventListener("click", (e) => {
+//     mobileNavbar.classList.toggle("display-block")
+//     mobileNavbarButton.classList.toggle("display-none")
+// })
+
+// mobileNavbarLink2.addEventListener("click", (e) => {
+//     mobileNavbar.classList.toggle("display-block")
+//     mobileNavbarButton.classList.toggle("display-none")
+// })
+
+// mobileNavbarLink3.addEventListener("click", (e) => {
+//     mobileNavbar.classList.toggle("display-block")
+//     mobileNavbarButton.classList.toggle("display-none")
+// })
+
+// mobileNavbarLink4.addEventListener("click", (e) => {
+//     mobileNavbar.classList.toggle("display-block")
+//     mobileNavbarButton.classList.toggle("display-none")
+// })
+
 const mobileNavbarButton = document.querySelector(".mobileNavbarButton")
 const mobileNavbar = document.querySelector(".mobileNavbar")
 const closeMobileNavbarButton = document.querySelector(".closeMobileNavbarButton")
-const mobileNavbarLink1 = document.getElementById("mobileNavbarLink1")
-const mobileNavbarLink2 = document.getElementById("mobileNavbarLink2")
-const mobileNavbarLink3 = document.getElementById("mobileNavbarLink3")
-const mobileNavbarLink4 = document.getElementById("mobileNavbarLink4")
+const mobileNavbarLink = document.querySelectorAll(".mobileNavbarLink")
 
 mobileNavbarButton.addEventListener("click", (e) => {
     mobileNavbar.classList.toggle("display-block")
@@ -16,22 +51,7 @@ closeMobileNavbarButton.addEventListener("click", (e) => {
     mobileNavbarButton.classList.toggle("display-none")
 })
 
-mobileNavbarLink1.addEventListener("click", (e) => {
-    mobileNavbar.classList.toggle("display-block")
-    mobileNavbarButton.classList.toggle("display-none")
-})
-
-mobileNavbarLink2.addEventListener("click", (e) => {
-    mobileNavbar.classList.toggle("display-block")
-    mobileNavbarButton.classList.toggle("display-none")
-})
-
-mobileNavbarLink3.addEventListener("click", (e) => {
-    mobileNavbar.classList.toggle("display-block")
-    mobileNavbarButton.classList.toggle("display-none")
-})
-
-mobileNavbarLink4.addEventListener("click", (e) => {
+mobileNavbarLink.addEventListener("click", (e) => {
     mobileNavbar.classList.toggle("display-block")
     mobileNavbarButton.classList.toggle("display-none")
 })
