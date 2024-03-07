289dece519b93d2fb805020ad11626019969f6e0
leandroheffes24 <leandroheffes@outlook.es>
2024-03-07 20:20:03 -0300

    old data to create proyect form added

--- src/middlewares/sessionExists.js
+++ src/middlewares/sessionExists.js
@@ -1,12 +1,8 @@
 function sessionExists (req, res, next) {
-    console.log("SESSION EXISTS MIDDLEWARE EXECUTED");
-
     res.locals.sessionExists = false
     if(req.session && req.session.userLoggedIn){
         res.locals.sessionExists = true
     }
-
-    console.log("ESTE ES EL ESTADO => " + res.locals.sessionExists);
 
     next()
 }
