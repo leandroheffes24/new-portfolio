477a3ffe790eb7d0e86db8a1a3e55a76bd67583e
leandroheffes24 <leandroheffes@outlook.es>
2024-03-08 15:37:39 -0300

    corrections

--- index.js
+++ index.js
@@ -14,25 +14,6 @@ const sessionExists = require("./src/middlewares/sessionExists")
 
 const app = express()
 
-// -------------------- MYSQL --------------------
-
-const mysql = require("mysql2")
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-})
-
-pool.getConnection((err, conn) => {
-    if(err) console.log(err)
-    console.log("Connected successfully")
-})
-
 // -------------------- CORS --------------------
 
 const cors = require("cors");
