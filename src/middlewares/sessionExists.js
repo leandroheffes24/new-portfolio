5152f898686ecfb1dcb255af6936ff2466b3420e
leandroheffes24 <leandroheffes@outlook.es>
2024-03-14 17:36:09 -0300

    styles corrections

--- src/middlewares/sessionExists.js
+++ src/middlewares/sessionExists.js
@@ -1,9 +1,7 @@
 function sessionExists (req, res, next) {
     res.locals.sessionExists = false
-    console.log("NO EXISTE SESSION");
     if(req.session && req.session.userLoggedIn){
         res.locals.sessionExists = true
-        console.log("EXISTE SESSION");
     }
 
     next()
