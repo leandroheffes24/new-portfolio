b9669098ca3973ed3b6ac2fee915b9ff6e9b617b
leandroheffes24 <leandroheffes@outlook.es>
2025-03-29 20:06:08 -0300

    home section complete

--- src/app/components/HomeSection/HomeSection.jsx
+++ src/app/components/HomeSection/HomeSection.jsx
@@ -1,5 +1,34 @@
+import Image from 'next/image'
+import styles from './HomeSection.module.css'
+import homeBackground from '../../assets/homeBackground.webp'
+import { poppins } from '../../../../public/fonts/fonts'
+import Link from 'next/link'
+
 export default function HomeSection(){
     return(
-        <div>home section</div>
+        <section className={styles.homeSection}>
+            <div className={styles.informationContainer}>
+                <h1 className={poppins.className}>Leandro Heffes</h1>
+                <h2>Full stack web developer</h2>
+
+                <div className={styles.buttonsContainer}>
+                    <button>
+                        <Link href={'#projects'}>Take a look at my projects</Link>
+                    </button>
+
+                    <button>
+                        <Link href={'#contact'}>Contact me</Link>
+                    </button>
+                </div>
+            </div>
+
+            <div className={styles.imageContainer}>
+                <Image
+                    src={homeBackground}
+                    alt='Home image background'
+                    className={styles.image}
+                />
+            </div>
+        </section>
     )
 }
