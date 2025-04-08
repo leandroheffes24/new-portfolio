0515c3dc7634c651e121c8b501d3391195d4cc1f
leandroheffes24 <leandroheffes@outlook.es>
2025-04-08 09:47:00 -0300

    basic structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+
+function App() {
+  return (
+    <>
+      {/* <Header/> */}
+      <main>
+        <Home/>
+      </main>
+    </>
+  )
+}
+
+export default App
