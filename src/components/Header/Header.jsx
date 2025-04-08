01e6bc8ff7d086b5169ad88b18dcb4ed5b08773c
leandroheffes24 <leandroheffes@outlook.es>
2025-04-08 10:54:02 -0300

    header and home for mobile and tabler complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
+import styles from './Header.module.css'
+
 export default function Header(){
     return(
-        <header>
-            header
-        </header>
+        <>
+            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={styles.openNavIcon}>
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M4 6h16" />
+                <path d="M7 12h13" />
+                <path d="M10 18h10" />
+            </svg>
+
+            {/* <header>
+                
+            </header> */}
+        </>
     )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,19 @@
+import styles from './home.module.css'
+
 export default function Home(){
     return(
-        <section>
-            home section
+        <section className={styles.homeSection}>
+            <div className={styles.container}>
+                <div className={styles.textContainer}>
+                    <h1>Leandro Heffes</h1>
+                    <h2>Full stack web developer</h2>
+                </div>
+
+                <div className={styles.linksContainer}>
+                    <a href="/projects">My projects</a>
+                    <a href="/contact">Contact</a>
+                </div>
+            </div>
         </section>
     )
 }
